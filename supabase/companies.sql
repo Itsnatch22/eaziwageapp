8ab5191eb50901934fc28d91d9186911ee477071
Itsnatch <kamaumark996@gmail.com>
2026-06-01 14:20:36 +0300

    12345

--- supabase/companies.sql
+++
@@ -1,7 +0,0 @@
-create table public.companies (
-  id uuid not null default gen_random_uuid (),
-  name text not null,
-  size integer null,
-  created_at timestamp without time zone null default now(),
-  constraint companies_pkey primary key (id)
-) TABLESPACE pg_default;
